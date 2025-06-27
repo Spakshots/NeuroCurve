2b962f09a694891693cfd2c94d04c774a36c2d00
AlphaLQ <sjb2019dpo@gmail.com>
2025-06-26 17:28:50 -0700

    filepath correction again

--- NeuroPy/NeuroCurveImports.py
+++ NeuroPy/NeuroCurveImports.py
@@ -3,4 +3,4 @@ import numpy as np
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
-NeuroCurveOG = pd.read_csv("CSV/TheChosenOne/alzheimers_disease_data.csv")
+NeuroCurveOG = pd.read_csv("CSV/alzheimers_disease_data.csv")
