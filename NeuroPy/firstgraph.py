8d7cb34456a3415917c26ffe22e0e56f8ee7ab7d
Spider7307 <spencer.oldoerp@lhsla.org>
2025-06-26 17:21:05 -0700

    First Graph

--- NeuroPy/firstgraph.py
+++ NeuroPy/firstgraph.py
@@ -1,4 +1,8 @@
 from NeuroCurveImports import *
 print(NeuroCurveOG.info())
 print(NeuroCurveOG.describe())
-print(NeuroCurveOG['BMI'].sum())
+#print(NeuroCurveOG['BMI'].sum())
+histo = plt.hist(NeuroCurveOG['Diagnosis'])
+#plt.ylim((0,10))
+plt.show()
+print(NeuroCurveOG['Diagnosis'].mean())
