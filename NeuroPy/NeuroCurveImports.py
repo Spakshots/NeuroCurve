8cb3daa931df8ddbfbf94b60bf909e8a1806c09a
Cap0703 <c.oldoerp26@bosco.org>
2025-07-01 14:30:44 -0700

    Some Graphing

--- NeuroPy/NeuroCurveImports.py
+++ NeuroPy/NeuroCurveImports.py
@@ -3,7 +3,21 @@ import numpy as np
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
+import seaborn as sns
 import csv
 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge, Lasso
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+
+
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import roc_curve, roc_auc_score
+from sklearn.metrics import precision_recall_curve
+
+os.chdir('NeuroCurve')
 NeuroCurveOG = pd.read_csv("CSV/alzheimers_disease_data.csv")
 usableCSV = pd.read_csv('CSV/output_CSVs/processed.csv')
+os.chdir('NeuroPy')
