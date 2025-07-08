a14e4754cec965b6f2e716981ca502251287f684
Cap0703 <c.oldoerp26@bosco.org>
2025-07-07 22:55:42 -0700

    Cleaning Up NeuroPy

--- NeuroPy/pre_process.py
+++
@@ -1,5 +0,0 @@
-from NeuroCurveImports import *
-"Drops any unnecessary features of the original dataset"
-def pre_process():
-    usableCSV = NeuroCurveOG.drop(columns=["PatientID", "Diabetes", "CardiovascularDisease", "DoctorInCharge", "Depression", "Depression", "SystolicBP", "DiastolicBP"])
-    usableCSV.to_csv('CSV/output_CSVs/processed.csv')
